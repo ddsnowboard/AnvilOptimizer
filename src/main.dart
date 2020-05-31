260f0eaa1f0f42a5223f9e626a0e9e6057b67c94
Will Koster <jameswk2@illinois.edu>
2020-05-30 21:21:41 -0500

    Something is broken
    Disaster. This is why people hate null so much.

--- src/main.dart
+++ src/main.dart
@@ -1,13 +1,22 @@
 import 'enchantments.dart';
 import 'items.dart';
 import "dart:math";
+import "dart:collection";
 
 void main() {
   var target = Sword(1, false, <Enchantment>{Smite(2)});
-  var sacrifice = Sword(1, false, <Enchantment>{Smite(3), Unbreaking(3)});
+  var sacrifice = Sword(1, false, <Enchantment>{Unbreaking(3)});
   var output = Sword(2, false, <Enchantment>{Smite(3), Unbreaking(3)});
-  print(_computeOutput(target, sacrifice));
-  print(output);
+  var orderings = allOrderings({
+    Book(0, false, {FireAspect(2)}),
+    Book(0, false, {Knockback(2)}),
+    target,
+    sacrifice
+  });
+  print(orderings.length);
+  for (final o in orderings) {
+    print(o);
+  }
 }
 
 class LevelTooHighException implements Exception {
@@ -18,6 +27,7 @@ class LevelTooHighException implements Exception {
 
 abstract class Enchantable {
   ConcreteEnchantable makeConcrete();
+  int getCost();
 }
 
 abstract class ConcreteEnchantable extends Enchantable {
@@ -28,6 +38,9 @@ abstract class ConcreteEnchantable extends Enchantable {
   Symbol get typeId;
   ConcreteEnchantable(this.priorWork, this.isDamaged, this.enchantments) {}
   ConcreteEnchantable clone();
+  // This is what it "costs" to just have this item. This ignores the cost of getting it.
+  // This is so I can calculate the total cost of a tree just by traversing it
+  int getCost() => 0;
   ConcreteEnchantable makeConcrete() {
     return this;
   }
@@ -118,12 +131,14 @@ class EnchantOutput {
 EnchantOutput combine(
     ConcreteEnchantable target, ConcreteEnchantable sacrifice) {
   var output = _computeOutput(target, sacrifice);
+  if (output == null) return null;
   var cost = _computeCost(target, sacrifice, output);
   return EnchantOutput(output, cost);
 }
 
 ConcreteEnchantable _computeOutput(
     ConcreteEnchantable target, ConcreteEnchantable sacrifice) {
+  if (!(target.typeId == sacrifice.typeId || sacrifice is Book)) return null;
   var out = target.clone();
   out.priorWork++;
   for (final enchant in sacrifice.enchantments) {
@@ -145,6 +160,7 @@ int _computeCost(ConcreteEnchantable target, ConcreteEnchantable sacrifice,
     [ConcreteEnchantable output]) {
   if (output == null) {
     output = _computeOutput(target, sacrifice);
+    if (output == null) return null;
   }
   return _priorWorkPenalty(target) +
       _priorWorkPenalty(sacrifice) +
@@ -189,20 +205,92 @@ Enchantment _findEnchantOfType(ConcreteEnchantable item, Enchantment other) {
 }
 
 class EnchantOrdering {
-    EnchantPairing lastPairing;
-    EnchantOrdering(this.lastPairing);
+  EnchantPairing lastPairing;
+  EnchantOrdering(this.lastPairing);
+  String toString() {
+    return "Cost: {this.getCost()} : ${this.lastPairing.toString()}";
+  }
+
+  bool isPossible() {
+    return this.getCost() != null;
+  }
+
+  int getCost() {
+    int out = 0;
+    Queue<Enchantable> q = Queue.from([lastPairing]);
+    while (q.isNotEmpty) {
+      var e = q.removeFirst();
+      var cost = e.getCost();
+      if (cost == null) return null;
+      out += e.getCost();
+      if (e is EnchantPairing) {
+        q.addLast(e.target);
+        q.addLast(e.sacrifice);
+      }
+    }
+    return out;
+  }
 }
 
 class EnchantPairing extends Enchantable {
   Enchantable target;
   Enchantable sacrifice;
+  EnchantOutput get _output {
+    // print(this.target.makeConcrete());
+    // print(this.sacrifice.makeConcrete());
+    this.__output ??=
+        combine(this.target.makeConcrete(), this.sacrifice.makeConcrete());
+    return this.__output;
+  }
+
+  EnchantOutput __output;
   EnchantPairing(this.target, this.sacrifice) {}
+  int getCost() {
+    return this._output?.cost;
+  }
+
   ConcreteEnchantable makeConcrete() {
-    return _computeOutput(
-        this.target.makeConcrete(), this.sacrifice.makeConcrete());
+    return this._output?.output;
+  }
+
+  String toString() {
+    return "(${this.target.toString()}) (${this.sacrifice.toString()})";
   }
 }
 
 Set<EnchantOrdering> allOrderings(Set<Enchantable> enchantables) {
-    // Nested for loops and recursion? 
+  List<Enchantable> es = enchantables.toList();
+  if (enchantables.length == 2) {
+    return {
+      EnchantOrdering(EnchantPairing(es[0], es[1])),
+      EnchantOrdering(EnchantPairing(es[1], es[0]))
+    };
+  } else if (enchantables.length <= 1) {
+    return {};
+  }
+  var out = <EnchantOrdering>{};
+
+  for (int i1 = 0; i1 < es.length; i1++) {
+    for (int i2 = 0; i2 < es.length; i2++) {
+      if (i1 == i2) continue;
+
+      List<Enchantable> newList = removeAtIdxs(es, <int>{i1, i2});
+      newList.add(EnchantPairing(es[i1], es[i2]));
+      out = out.union(allOrderings(newList.toSet()));
+    }
+  }
+  var f = out.where((e) {
+    print(e);
+    return e.isPossible();
+  }).toSet();
+  print("HERE");
+  return f;
+}
+
+List<Enchantable> removeAtIdxs(List<Enchantable> es, Set<int> idxs) {
+  List<Enchantable> out = [];
+  for (int i = 0; i < es.length; i++) {
+    if (!idxs.contains(i)) out.add(es[i]);
+  }
+  return out;
 }
